6e3604a0872be7852ca4bf836cd24c07aa7ffd63
Pratyas Kumar <103165258+partyas8789@users.noreply.github.com>
2022-09-17 09:25:07 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (89.13%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,5 +1,29 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        if(n==0){
+            return 1;
+        }
+        if(n>0){
+            return posmyPow(x,n);
+        }else{
+            return negmyPow(x,n);
+        }
+    }
+    public double posmyPow(double x, int n){
+        if(n==0){
+            return 1;
+        }
+        double faith= posmyPow(x,n/2);
+        if(n%2==0){
+            return faith*faith;
+        }else{
+            return x*faith*faith;
+        }
+    }
+    
+    public double negmyPow(double x, int n){
+        n=n*-1;
+        double ans=posmyPow(x,n);
+        return (1/ans);
     }
 }

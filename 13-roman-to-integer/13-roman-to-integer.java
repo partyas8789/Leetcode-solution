37e34d7a3197cf14dc44ec7f29cd837c9e09b987
Pratyas Kumar <103165258+partyas8789@users.noreply.github.com>
2022-10-06 22:19:26 +0530

    Time: 7 ms (74.50%), Space: 45.2 MB (69.59%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,29 +1,27 @@
 class Solution {
-    public int romanToInt(String s) {
-        HashMap<Character,Integer>hm=new HashMap<>();
-        hm.put('I',1);
-        hm.put('V',5);
-        hm.put('X',10);
-        hm.put('L',50);
-        hm.put('C',100);
-        hm.put('D',500);
-        hm.put('M',1000);
-        int sum=0;
-        Stack<Integer>st=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(st.isEmpty()) st.push(hm.get(ch));
-            else{
-                int temp=hm.get(ch);
-                if(st.peek()<temp) {
-                    int a=st.pop();
-                    st.push((a*-1));
-                    st.push((temp));
+    public int romanToInt(String str) {
+        int[] integerArr = { 1000, 500,  100,  50,  10,  5, 1 };
+        char[] romanArr = {'M', 'D', 'C', 'L', 'X', 'V', 'I' };
+        int ans=0;
+        int temp=0;
+        for(int i=str.length()-1;i>=0;i--){
+            char ch=str.charAt(i);
+            for(int j=0;j<romanArr.length;j++){
+                if(ch==romanArr[j]){
+                    int val = integerArr[j];
+                    if (val >= temp){
+                    ans += val;
+                    } else {
+                        ans -= val;
+                    }
+                    temp = val;
                 }
-                else st.push((temp));
+                
+                
             }
-        }while(!st.isEmpty()){
-            sum=sum+st.pop();
-        }return sum;
+                
+        }
+        
+        return ans;
     }
 }

36a560b90541c0b80d5fea4fa51b977ac907e049
Pratyas Kumar <103165258+partyas8789@users.noreply.github.com>
2022-10-07 17:26:57 +0530

    Time: 9 ms (11.48%), Space: 42.5 MB (10.88%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countPoints(String rings) {
+        HashSet<Integer>hs1=new HashSet<>();
+        HashMap<Integer,HashSet<Character>>hm=new HashMap<>();
+        for(int i=0;i<rings.length()-1;i=i+2){
+            int num=rings.charAt(i+1)-'0';
+            char ch1=rings.charAt(i);
+            if(hm.containsKey(num)){
+                HashSet<Character>hs=hm.get(num);
+                hs.add(ch1);
+                if(hs.size()==3){
+                    hs1.add(num);
+                }
+                hm.put(num,hs);
+            }else{
+                HashSet<Character>hs=new HashSet<>();
+                hs.add(ch1);
+                if(hs.size()==3){
+                    hs1.add(num);
+                }
+                hm.put(num,hs);
+            }
+        }System.out.println(hm);
+        System.out.print(hs1);
+        return hs1.size();
+    }
+}
